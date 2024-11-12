7ecefee970a0c361e76aa79e045be63b705ad9fa
Victor Maciel <victormaciel.dev@gmail.com>
2024-11-12 09:55:49 -0300

    up

--- automacao-certificados/app.py
+++ automacao-certificados/app.py
@@ -10,20 +10,31 @@ for indice, linha in enumerate(sheet_alunos.iter_rows(min_row=2)):
     nome_curso = linha[0].value
     nome_participante = linha[1].value
     tipo_participacao = linha[2].value
+    carga_horaria = linha[5].value
+
     data_inicio = linha[3].value
     data_final = linha[4].value
-    carga_horario = linha[5].value
+    
     data_emissao = linha[6].value
     
 
     # Transferir para a imagem do certificado
     # Definindo a fonte a ser usada
-    fonte_nome = ImageFont.truetype('./TAHOMABD.TTF')
-    fonte_geral = ImageFont.truetype('./TAHOMA.TTF')
+    fonte_nome = ImageFont.truetype('./TAHOMABD.TTF',90)
+    fonte_geral = ImageFont.truetype('./TAHOMA.TTF',80)
+    fonte_data = ImageFont.truetype('./TAHOMA.TTF',55)
 
     image = Image.open('./certificado_padrao.jpg')
     desenhar = ImageDraw.Draw(image)
 
-    desenhar.text((200,600),nome_participante,fill='black', font=fonte_nome)
+    desenhar.text((1020,827),nome_participante,fill='black', font=fonte_nome)
+    desenhar.text((1060,950), nome_curso, fill='black', font=fonte_geral)
+    desenhar.text((1435,1065), tipo_participacao, fill='black', font=fonte_geral)
+    desenhar.text((1490,1185), str(carga_horaria) + ' h', fill='black', font=fonte_geral)
+
+    desenhar.text((750,1770),data_inicio, fill='blue', font=fonte_data)
+    desenhar.text((750,1930), data_final, fill='blue', font=fonte_data)
+
+    desenhar.text((2220, 1930), data_emissao, fill='blue', font=fonte_data)
 
     image.save(f'./{indice} {nome_participante} certificado.png')
