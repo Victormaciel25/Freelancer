62f4797bae423292fb7f9fb26d795feabd5cd06f
Victor Maciel <victormaciel.dev@gmail.com>
2024-11-16 22:53:50 -0300

    up

--- Projeto Django 1/projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ Projeto Django 1/projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     # rota, view responsável, nome de referência
     # usuarios.com
     path('', views.home, name='home'),
+    # usuarios.com/usuarios
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
 ]
